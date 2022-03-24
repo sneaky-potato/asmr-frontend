0210ca6585bed2b3ab5e7b165ac4709ef304795d
sneaky-potato <rajivkamal.im421@gmail.com>
2022-03-24 18:34:02 +0530

    add pages for doctor and patient

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./views/landingPage";
 import MainPage from "./views/mainPage";
 import Hospitals from "./views/hospitals";
-import RegisterCard from "./components/registerCard";
+import DoctorRegisterCard from "./components/doctorRegisterCard";
+import PatientRegisterCard from "./components/patientRegisterCard";
 import LoginCard from "./components/loginCard";
 import ProfilePage from "./views/profilePage";
 
@@ -14,7 +15,8 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/ocms" element={<MainPage />} />
           <Route path="/ocms/hospitals" element={<Hospitals />} />
-          <Route path="/ocms/register" element={<RegisterCard />} />
+          <Route path="/ocms/register/doctor" element={<DoctorRegisterCard />} />
+          <Route path="/ocms/register/patient" element={<PatientRegisterCard />} />
           <Route path="/ocms/login" element={<LoginCard />} />
           <Route path="/ocms/me" element={<ProfilePage />} />
         </Routes>
