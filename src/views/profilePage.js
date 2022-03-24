4b04d03a3c34de7cdec6dc176f9aebeec987fd49
sneaky-potato <rajivkamal.im421@gmail.com>
2022-03-24 18:34:47 +0530

    create profile page

--- src/views/profilePage.js
+++ src/views/profilePage.js
@@ -12,27 +12,71 @@ const ProfilePage = () => {
   const [userDetail, setUserDetail] = useState({});
 
   useEffect(() => {
-
       if (localStorage.getItem("userDetail") !== null) setUserDetail(JSON.parse(localStorage.getItem("userDetail")));
-      console.log(localStorage.getItem("access_token"))
       var access = JSON.parse(localStorage.getItem("access_token"))
-      console.log('Authorization', 'Bearer ' + access)
-      axios.get(`${BACKEND_URL}/api/auth/me`, {
+
+      axios.get(`${BACKEND_URL}/api/auth/ping`, {
         headers: {
-            'content-type': 'application/json',
-            'Authorization': 'Bearer ' + access
+          'content-type': 'application/json',
+          'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem("access_token"))
         }
+      }).then((response) => {
+        console.log("redirct from ping");
+        axios.get(`${BACKEND_URL}/api/auth/me`, {
+          headers: {
+              'content-type': 'application/json',
+              'Authorization': 'Bearer ' + access
+          }
+      })
+            .then((response) => {
+                console.log("response =", response)
+                setUserDetail(response.data.me)
+            })
+            .catch((err) => console.log(err))
     })
-          .then((response) => {
-              console.log(response)
-              setUserDetail(response.data.me)
-          })
-          .catch((err) => console.log(err))
+    .catch((err) => {
+      console.log(err);
+      if (err.response.status == 401) {
+        axios.post(`${BACKEND_URL}/api/auth/token/refresh/`, {
+          refresh: JSON.parse(localStorage.getItem("refresh_token"))
+        })
+        .then((response) => {
+          console.log("redirected from refresh")
+          console.log(response);
+          localStorage.setItem("access_token", JSON.stringify(response.data.access));
+          axios.get(`${BACKEND_URL}/api/auth/me`, {
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': 'Bearer ' + access
+            }
+        })
+              .then((response) => {
+                  console.log("response =", response)
+                  setUserDetail(response.data.me)
+              })
+              .catch((err) => console.log(err))
+
+        })
+        .catch((err) => {
+          console.log(err);
+          // Handle refresh token expiration
+          
+
+          
+        })
+      }
+    });
   }, []);
 
   localStorage.setItem("userDetail", JSON.stringify(userDetail));
-
-  // if(userDetail.role == 1) return (<)
+  console.log("details =", userDetail)
+  
+  return(
+    <div className="profile-page">
+        { userDetail.role == 1 ? <AdminPage /> : (userDetail.role == 2 ? <DoctorPage /> : <PatientPage />) }
+    </div>
+  )
+  
 }
 
 export default ProfilePage;
