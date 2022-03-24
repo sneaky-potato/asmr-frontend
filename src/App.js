edd5345762e88956bc92f6485c8b8f03c507c9ea
sneaky-potato <rajivkamal.im421@gmail.com>
2022-03-24 10:51:40 +0530

    create basic views and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./views/landingPage";
+import MainPage from "./views/mainPage";
+import Hospitals from "./views/hospitals";
+import RegisterCard from "./components/registerCard";
+import LoginCard from "./components/loginCard";
+// import { AuthProvider } from "./contexts/authContext";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        {/* <AuthProvider> */}
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/ocms" element={<MainPage />} />
+          <Route path="/ocms/hospitals" element={<Hospitals />} />
+          <Route path="/ocms/register" element={<RegisterCard />} />
+          <Route path="/ocms/login" element={<LoginCard />} />
         </Routes>
+        {/* </AuthProvider> */}
       </BrowserRouter>
     </div>
   );
