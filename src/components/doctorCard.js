fb01a38325db9a65f71d969f7ec3efef7c558d60
sneaky-potato <sneakypotato.hello@gmail.com>
2022-03-30 13:30:20 +0530

    fix appointment scheduling request

--- src/components/doctorCard.js
+++ src/components/doctorCard.js
@@ -1,6 +1,16 @@
 import CustomAxios from "../utils/customAxios";
+import { Notyf } from "notyf";
+import { Navigate, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const DoctorCard = (props) => {
+
+  const notyf = new Notyf();
+  const navigate = useNavigate();
+  let hospitals = JSON.parse(localStorage.getItem("hospitalList")).find((hospital, index) => {
+    return hospital.id == props.hospital
+  })
+  
     const user = JSON.parse(localStorage.getItem("userDetail"));
     console.log(user)
     async function handleAppointment(event) {
@@ -8,7 +18,7 @@ const DoctorCard = (props) => {
       CustomAxios.post('appointments', {
         doctor_id: props.id, 
         patient_id: user.id,
-        description: 'lodupanti',
+        description: 'Scheduled Appointment',
         date: (props.date).toISOString().slice(0,10)
       }).then((response) => {
         console.log("appointment creted =", response);
@@ -44,19 +54,33 @@ const DoctorCard = (props) => {
           }) 
     }
 
+    async function handleRemove(event) {
+      event.preventDefault();
+      CustomAxios.delete(`users/${props.id}`, {})
+      .then((response) => {
+        console.log(response)
+        window.location.reload(true)
+        notyf.success("User remove from database")
+
+      }).catch((err) => {
+        console.log(err)
+        notyf.error("Something went wrong")
+      })
+    }
+
     return (
         <div className="doctor-card">
             <div className="doctor-title">Dr. {props.name} {props.lastname}</div>
             {/* <div className="doctor-title-surname"></div> */}
-            <div className="doctor-hospital">{props.hospital}</div>
-            <div className="doctor-speciality">{props.speciality}</div>
+            <div className="doctor-hospital">Hospital: {hospitals.name + ", " + hospitals.address}</div>
+            <div className="doctor-speciality">Speciality: {props.speciality}</div>
             <div className="doctor-address">{props.address}</div>
             <div className="doctor-pincode">{props.pincode}</div>
             <div className="doctor-contact">{props.contact}</div>
             <div className="doctor-email">{props.email}</div>
             {
               props.byAdmin ? ( props.toAccept ? <button className="navbar-button button" onClick={handleAccept}>Accept</button> 
-              : <button className="navbar-button button" onClick={handleAccept}>Remove</button> )
+              : <button className="navbar-button button" onClick={handleRemove}>Remove</button> )
               : <button className="navbar-button button" onClick={handleAppointment}>Book appointment</button>
             }
         </div>
